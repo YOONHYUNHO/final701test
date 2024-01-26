0e2f1a4b02710a43173138d76819532126178623
YOONHYUNHO <dbsgusgh2004@naver.com>
2024-01-26 16:08:53 +0900

    세번쨰

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import mainImg from './image/24.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={mainImg} style={{ width: '650px' }} alt="" />
+      <h2><b>스프링부트 + 리액트 CI/CD</b></h2>
+    </div>
+  );
+}
+
+export default App;
